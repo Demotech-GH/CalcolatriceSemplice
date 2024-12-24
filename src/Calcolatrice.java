147d48d4565767933bf7a41347b7377ea7d75283
DiegoD <diego.demori01@gmail.com>
2024-12-24 10:50:45 +0100

    lesgo

--- src/Calcolatrice.java
+++ src/Calcolatrice.java
@@ -1,3 +1,76 @@
+import java.util.Scanner;
+
 public class Calcolatrice
 {
+	static Scanner sc = new Scanner(System.in);
+
+	public static void main(String[] args)
+	{
+		System.out.println("Benvenuto nella Calcolatrice Semplice!" + "\n" + "Scegli un'operazione:+,-,*,/");
+		String operazione = sc.nextLine();
+
+		System.out.println("Inserisci il primo numero: ");
+		double a = Double.parseDouble(sc.nextLine());
+
+		System.out.println("Inserisci il secondo numero: ");
+		double b = Double.parseDouble(sc.nextLine());
+
+
+		double risultato = 0;
+
+//		switch (operazione)
+//		{
+//			case "+" -> risultato = somma(a,b);
+//			case "-" -> risultato = sottrazione(a,b);
+//			case "*" -> risultato = moltiplicazione(a,b);
+//			case "/" -> risultato = divisione(a,b);
+//			default -> System.out.println("inserire una operazione valida");
+//
+//		}
+
+		try
+		{
+			risultato = switch (operazione)
+			{
+				case "+" -> somma(a, b);
+				case "-" -> sottrazione(a, b);
+				case "*" -> moltiplicazione(a, b);
+				case "/" -> divisione(a, b);
+
+
+				default -> throw new IllegalArgumentException("Unexpected value: " + operazione);
+			};
+
+			System.out.println("Il risultato è: " + risultato);
+		}catch(Exception e)
+		{
+			System.out.println("non va bene quello che hai messo mannaggina");
+		}
+
+	}
+
+	private static double divisione(double a, double b)
+	{
+		return a / b;
+	}
+
+	private static double moltiplicazione(double a, double b)
+	{
+		return a * b;
+	}
+
+	private static double sottrazione(double a, double b)
+	{
+		return a - b;
+	}
+
+	public static double somma(double a, double b)
+	{
+
+		return a + b;
+
+	}
+
+
+
 }
---
+++ src/Exception/SantaGiftNotDeliveredException.java
@@ -0,0 +1,9 @@
+package Exception;
+
+public class SantaGiftNotDeliveredException extends RuntimeException
+{
+	public SantaGiftNotDeliveredException(String message)
+	{
+		super(message);
+	}
+}
